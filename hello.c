a302ed7664b3951ad22bc00a5857a2831550dc71
mktmkt141 <makotunn0714@icloud.com>
2025-05-10 16:03:05 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,10 +3,16 @@
 
 int main(int argc, char **argv){
     int rank, size;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    printf("Hello World, I am %d of %d\n", rank, size);
+    MPI_Get_processor_name(processor_name, &name_len);  // ← 追加
+
+    printf("Hello World, I am %d of %d running on %s\n", rank, size, processor_name);
+
     MPI_Finalize();
     return 0;
 }
