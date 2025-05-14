b01c0b1df30075d5efe9e401c02ddb9646b4849a
mktmkt141 <makotunn0714@icloud.com>
2025-05-15 00:38:16 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <unistd.h> // gethostname 関数を使うために必要
 
 int main(int argc, char **argv){
     int rank, size;
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
+    char hostname[256]; // ホスト名を格納する配列
 
     MPI_Init(&argc, &argv);
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Get_processor_name(processor_name, &name_len);  // ← 追加
 
-    printf("Hello World, I am %d of %d running on %s\n", rank, size, processor_name);
+    // ホスト名を取得
+    gethostname(hostname, sizeof(hostname));
+    hostname[sizeof(hostname) - 1] = '\0'; // Null終端を保証
+
+    printf("Hello World from %s, I am rank %d of %d\n", hostname, rank, size);
 
     MPI_Finalize();
-    return 0;
+    return(0);
 }
